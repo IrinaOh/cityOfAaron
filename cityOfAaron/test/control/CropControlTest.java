5f5d5d4f705bf297490bc4bed2fdc4f4abb93662
Juppy89 <35502991+Juppy89@users.noreply.github.com>
2018-10-25 17:04:44 -0600

    Justin Wakefield Start of buyLand method

--- cityOfAaron/test/control/CropControlTest.java
+++ cityOfAaron/test/control/CropControlTest.java
@@ -5,6 +5,7 @@
  */
 package control;
 
+import model.CropData;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -41,6 +42,23 @@ public class CropControlTest {
         CropData cropData = null;
         int expResult = 0;
         int result = CropControl.sellLand(landPrice, acresToSell, cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of buyLand method, of class CropControl.
+     */
+    @Test
+    public void testBuyLand() {
+        System.out.println("buyLand");
+        int landPrice = 0;
+        int acresToBuy = 0;
+        CropData cropData = null;
+        CropControl instance = new CropControl();
+        int expResult = 0;
+        int result = instance.buyLand(landPrice, acresToBuy, cropData);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
