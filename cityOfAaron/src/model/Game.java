a69e4cc6e3769c8c89d300b6b9ac419bbcef3668
Irina O'Hara <irina.v.ohara@gmail.com>
2018-11-01 21:38:29 -0600

    Irina O'Hara view

--- cityOfAaron/src/model/Game.java
+++ cityOfAaron/src/model/Game.java
@@ -6,6 +6,7 @@
 */
 package model;
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -13,9 +14,22 @@ import java.io.Serializable;
  */
 public class Game implements Serializable{
     private Player player;
+    private Game game;
     
     public Game(){}
 
+    public Game(Game game) {
+        this.game = game;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+    
     public Player getPlayer() {
         return player;
     }
@@ -23,6 +37,48 @@ public class Game implements Serializable{
     public void setPlayer(Player player) {
         this.player = player;
     }
+
+    public void setCropData(CropData cropData) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.player);
+        hash = 59 * hash + Objects.hashCode(this.game);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (!Objects.equals(this.player, other.player)) {
+            return false;
+        }
+        if (!Objects.equals(this.game, other.game)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "player=" + player + ", game=" + game + '}';
+    }
+    
+
+
+
     
 }
