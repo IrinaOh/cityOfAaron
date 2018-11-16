d7b92281505d7124d157c27d6a55eb71f0dac4b8
Irina O'Hara <irina.v.ohara@gmail.com>
2018-11-15 22:48:45 -0700

    Irina O'Hara HelpMenuView

--- cityOfAaron/src/view/HelpMenuView.java
+++ cityOfAaron/src/view/HelpMenuView.java
@@ -11,37 +11,9 @@ import java.util.Scanner;
  *
  * @author irina
  */
-public class HelpMenuView {
+public class HelpMenuView extends MenuView{
     
     Scanner keyboard = new Scanner(System.in);
-    private final String theHelpMenu;
-    private final int max;
-    
-    /**
-    * The displayHelpView method
-    * Purpose: displays the menu, gets the user's input, and does the
-    *   selected action
-    * Parameters: none
-    * Returns: none
-    */
-    // =========================================================
-    public void displayMenuView()
-    {
-        int menuOption;
-        do
-        {
-            // Display the menu
-            System.out.println(theHelpMenu);
-            
-            // Prompt the user and get the user’s input
-            menuOption = getMenuOption();
-            
-            // Perform the desired action
-            doAction(menuOption);
-            
-            // Determine and display the next view
-        } while (menuOption != max);
-     }
     
     /**
     * The HelpMenuView constructor
@@ -52,7 +24,7 @@ public class HelpMenuView {
     // ===================================
     public HelpMenuView()
     {
-        theHelpMenu = "\n" +
+        super("\n" +
             "**********************************\n" +
             "* CITY OF AARON: HELP MENU *\n" +
             "**********************************\n" +
@@ -61,42 +33,9 @@ public class HelpMenuView {
             " 3 - How do I view the map?\n" +
             " 4 - How do I move to another location?\n" +
             " 5 - How do I display a list of animals, provisions, and tools in the city storehouse?\n" +
-            " 6 - Back to the Main Menu.\n";
-
-        max = 6;
+            " 6 - Back to the Main Menu.\n",
+            6);
     }  
-    
-    /**
-    * The getMenuOption method
-    * Purpose: gets the user's input
-    * Parameters: none
-    * Returns: integer - the option selected
-     * @return 
-    */
-    // ===================================
-    public int getMenuOption()
-    {
-        // declare a variable to hold user’s input
-        int userInput;
-        
-        // begin loop
-        do
-        {    
-            // get user input from the keyboard
-            userInput = keyboard.nextInt();
-            
-            // if it is not a valid value, output an error message
-            if (userInput < 1 || userInput > max)
-            {
-            System.out.println("Option must be between 1 and " + max);
-            }
-            
-        // loop back to the top if input was not valid
-        } while(userInput < 1 || userInput > max);
-        
-        // return the value input by the user
-        return userInput;
-    }
     
     /**
     *The doAction method
@@ -106,6 +45,7 @@ public class HelpMenuView {
      * @param option
     */
     // ===================================
+    @Override
     public void doAction(int option)
     {
         switch (option)
@@ -145,14 +85,14 @@ public class HelpMenuView {
     }
 
     private void viewMapHelp() {
-    System.out.println("Enter 6 to RETURN TO THE MAIN MENU view, start a new game, and enter 1 to display the map");    
+    System.out.println("To display the map enter 6 to RETURN TO THE MAIN MENU view, start a new game, and enter 1 ");    
     }
 
     private void viewMoveHelp() {
-    System.out.println("Enter 6 to RETURN TO THE MAIN MENU view, start a new game, and enter 3 to move to a new location.");    
+    System.out.println("To move to a new location enter 6 to RETURN TO THE MAIN MENU view, start a new game, and enter 3.");    
     }
 
     private void viewListHelp() {
-    System.out.println("Enter 6 to RETURN TO THE MAIN MENU view, start a new game or continue a saved one, and enter options 1-4 to display resources.");    
+    System.out.println("To display resources enter 6 to RETURN TO THE MAIN MENU view, start a new game or continue a saved one, and enter options 1-4.");    
     }
 }
